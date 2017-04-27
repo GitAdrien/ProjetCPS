e3e6033bdca5d26779a5bc5ccc67339bf8374d85
MISS <3605386@ppti-14-502-04.ufr-info-p6.jussieu.fr>
2017-04-27 13:36:43 +0200

    juste un peu plus

--- src/test/AbstractEngine.java
+++ src/test/AbstractEngine.java
@@ -1,9 +1,11 @@
 package test;
 
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
+import contract.errors.PostConditionError;
 import contract.errors.PreConditionError;
 import interfaceservice.CharacterService;
 import interfaceservice.EngineService;
@@ -31,25 +33,158 @@ public abstract class AbstractEngine {
 		p1.init(10, c1);
 		p2.init(100, c2);
 	}
-	
+
 	@After
 	public final void afterTests(){
-		
+
 	}
-	
-	
+
+
 	//fonction init
 	//PRE
-	
+
 	@Test
-	public void testInit(){
+	public void testInitPre(){
 		try{
-		engine.init(300, 1000, 100, p1, p2);
+			engine.init(300, 1000, 100, p1, p2);
+			Assert.assertTrue(true);
 		}
 		catch(PreConditionError p){
-			
+			Assert.assertTrue(false);
 		}
 	}
 
+	@Test
+	public void testInitPreFail(){// h < 0
+		try{
+			engine.init(-1, 1000, 100, p1, p2);
+			Assert.assertTrue(false);
+		}
+		catch(PreConditionError p){
+			Assert.assertTrue(true);
+		}
+	}
+	
+	@Test
+	public void testInitPreFail2(){ //w<s
+		try{
+			engine.init(300, 5, 100, p1, p2);
+			Assert.assertTrue(false);
+		}
+		catch(PreConditionError p){
+			Assert.assertTrue(true);
+		}
+	}
+	
+	@Test
+	public void testInitPreFail3(){ // p1 == p2
+		try{
+			engine.init(300, 1000, 100, p2, p2);
+			Assert.assertTrue(false);
+		}
+		catch(PreConditionError p){
+			Assert.assertTrue(true);
+		}
+	}
 
+	@Test
+	public void testInitPreFail4(){ // s<0
+		try{
+			engine.init(300, -5, 100, p2, p2);
+			Assert.assertTrue(false);
+		}
+		catch(PreConditionError p){
+			Assert.assertTrue(true);
+		}
+	}
+	
+	//POST
+	
+	@Test
+	public void testInitPostHeight(){ 
+		try{
+			engine.init(300, 1000, 100, p1, p2);
+			Assert.assertTrue(engine.height() == 300);
+		}
+		catch(PostConditionError p){
+			Assert.assertTrue(false);
+		}
+	}
+	
+	@Test
+	public void testInitPostHeightFail(){ 
+		try{
+			engine.init(300, 1000, 100, p1, p2);
+			Assert.assertTrue(engine.height() != 300);
+		}
+		catch(PostConditionError p){
+			Assert.assertTrue(false);
+		}
+	}
+	
+	@Test
+	public void testInitPostWidth(){ 
+		try{
+			engine.init(300, 1000, 100, p1, p2);
+			Assert.assertTrue(engine.width() == 1000);
+		}
+		catch(PostConditionError p){
+			Assert.assertTrue(false);
+		}
+	}
+	
+	@Test
+	public void testInitPostWidthFail(){ 
+		try{
+			engine.init(300, 1000, 100, p1, p2);
+			Assert.assertTrue(engine.width() != 1000);
+		}
+		catch(PostConditionError p){
+			Assert.assertTrue(false);
+		}
+	}
+	
+	@Test
+	public void testInitPostPlayer(){ 
+		try{
+			engine.init(300, 1000, 100, p1, p2);
+			Assert.assertTrue(engine.player(1) == p1);
+		}
+		catch(PostConditionError p){
+			Assert.assertTrue(false);
+		}
+	}
+	
+	@Test
+	public void testInitPostPlayer2(){ 
+		try{
+			engine.init(300, 1000, 100, p1, p2);
+			Assert.assertTrue(engine.player(2) == p2);
+		}
+		catch(PostConditionError p){
+			Assert.assertTrue(false);
+		}
+	}
+	
+	@Test
+	public void testInitPostPlayerFail(){ 
+		try{
+			engine.init(300, 1000, 100, p1, p2);
+			Assert.assertTrue(!(engine.player(1) == p2));
+		}
+		catch(PostConditionError p){
+			Assert.assertTrue(false);
+		}
+	}
+	
+	@Test
+	public void testInitPostPlayerFail2(){ 
+		try{
+			engine.init(300, 1000, 100, p1, p2);
+			Assert.assertTrue(!(engine.player(2) == p1));
+		}
+		catch(PostConditionError p){
+			Assert.assertTrue(false);
+		}
+	}
 }
