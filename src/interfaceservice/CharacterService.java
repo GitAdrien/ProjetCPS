57825d2ffe1151bb2c658b9afc28da6da16637cd
GitAdrien <adrien.miss@gmail.com>
2017-05-04 17:00:16 +0200

    petit ajout

--- src/interfaceservice/CharacterService.java
+++ src/interfaceservice/CharacterService.java
@@ -12,7 +12,7 @@ public interface CharacterService {
 	public HitboxService charBox();
 	public int life();
 	public int maxLife();
-	public int speed();
+	public int speed(); //const
 	public int gravity();
 	public int jumpSpeed();
 	public boolean faceRight();
@@ -52,11 +52,11 @@ public interface CharacterService {
 	//				Hitbox::collidesWith(charBox(moveLeft(c)), charBox(Engine::player(engine(c), i)))
 	//				\implique positionX(moveLeft(c)) = positionX(c)
 	// \post : positionX(c) <= speed(c) \and
-	//			(\exists i \with Engine::player(engine(c), i) != c \implique  
+	//			(\some i \with Engine::player(engine(c), i) != c \implique  
 	//				non(Hitbox::collidesWith(hitbox(moveLeft(c)),charBox(Engine::player(engine(c),i))))
 	//			\implique positionX(moveLeft(c)) = 0
 	// \post positionX(c) > speed(c) \and
-	//			(\exists i \with Engine::player(engine(c), i) != c \implique  
+	//			(\some i \with Engine::player(engine(c), i) != c \implique  
 	//				non(Hitbox::collidesWith(hitbox(moveLeft(c)),charBox(Engine::player(engine(c),i))))
 	//			\implique positionX(moveLeft(c)) = positionX(c) - speed(c)
 	// \post faceRight(moveLeft(c)) = faceRight(c) \and life(moveLeft(c)) = life(c)
@@ -67,37 +67,45 @@ public interface CharacterService {
 	//				Hitbox::collidesWith(charBox(moveRight(c)), charBox(Engine::player(engine(c), i)))
 	//				\implique positionX(moveRight(c)) = positionX(c)
 	// \post Engine::width(engine(c)) <= speed(c) + positionX(c) \and
-	//			(\exists i \with Engine::player(engine(c), i) != c \implique  
+	//			(\some i \with Engine::player(engine(c), i) != c \implique  
 	//				non(Hitbox::collidesWith(hitbox(moveRight(c)),charBox(Engine::player(engine(c),i))))
 	//			\implique positionX(moveRight(c)) = positionX(c) + speed(c)
 	// \post Engine::width(engine(c)) > speed(c) + positionX(c) \and
-	//			(\exists i \with Engine::player(engine(c), i) != c \implique  
+	//			(\some i \with Engine::player(engine(c), i) != c \implique  
 	//				non(Hitbox::collidesWith(hitbox(moveRight(c)),charBox(Engine::player(engine(c),i))))
 	//			\implique positionX(moveRight(c)) = Engine::width(engine(c))
-	// \post faceRight(moveLeft(c)) = faceRight(c) \and life(moveRight(c)) = life(c)
-	// \post positionY(moveLeft(c)) = positionY(c)
+	// \post faceRight(moveRight(c)) = faceRight(c) \and life(moveRight(c)) = life(c)
+	// \post positionY(moveRight(c)) = positionY(c)
 	public CharacterService moveRight();
 
 	// \post faceRight(switchSide(c)) != faceRight(c)
 	// \post positionX(switchSide(c)) = positionX(c)
 	public CharacterService switchSide();
 
-	// TODO
+	// \post :  \no(crouched(c)) \implique ((positionX(crouch(c) = positionX(c)) \and (positionY(crouch(c)) = positionY(c) - Hitbox::height(charBox(c))/2) \and
+	// Hitbox::height(charBox(c))/2 = height(charBox(crouch(c)))
+	// \post : (crouched(c)) \implique c = crouch(c)
 	public CharacterService crouch();
 	
-	// TODO
+	// \post : crouched(c) \implique ((positionX(standUp(c) = positionX(c)) \and (positionY(standUp(c)) = positionY(c) + Hitbox::height(charBox(c))) \and
+	// Hitbox::height(charBox(c))*2 = Hitbox::height(charBox(standUp(c)))
+	// \post : \no(crouched(c)) \implique c = standUp(c)
 	public CharacterService standUp();
 
 	// TODO direction : -1=gauche, 0=pas de direction, 1=droite
+	// \post : \some i \in {-1, 0, 1} jumping(c) \implique c = jump(c, i)
 	public CharacterService jump(int direction);
 	
 	// TODO
 	public CharacterService useTechnic(List<Command> commands);
 	
 	// TODO Permet de decaler le joueur en cas de collision avec un autre joueur.
+	//collision venant de la gauche
+	// \post : positionX(bumpLeft(c)) = min(Engine::width, positionX(C) + speed(C)/2) 
 	public CharacterService bumpLeft();
 	
 	// TODO
+	// \post : positionX(bumpRight(c)) = max(0, positionX(C) - speed(C)/2) 
 	public CharacterService bumpRight();
 	
 	
@@ -105,7 +113,13 @@ public interface CharacterService {
 	// \post : step(c, LEFT) = moveLeft(c)
 	// \post : step(c, RIGHT) = moveRight(c)
 	// \post : step(c, NEUTRAL) = c
-	// TODO ajouté les commandes avancées 
+	// \post : step(c, DOWN) = crouch(c)
+	// \post : step(c, UP) = jump(c, 0)
+	// \post : step(c, DOWN_LEFT) = crouch(c)
+	// \post : step(c, DOWN_RIGHT) = crouch(c)
+	// \post : step(c, UP_LEFT) = jump(c, -1)
+	// \post : step(c, UP_RIGHT) = jump(c, 1)
+	//TODO : commande d'attaque???
 	public CharacterService step(Command com);
 	
 	
@@ -113,7 +127,10 @@ public interface CharacterService {
 	// \post addTechnic(c, t) \implique t \in technics(c)
 	public void addTechnic(TechnicService t);
 
-	// TODO
+	// \pre : takeDamages(a, s) \require a >=0 \and s >= 0
+	// \post : life(takeDamages(c, a, s)) = life(c) - a
+	// \post : ((s > 0) \and stunned(takeDamages(c, a, s))) = true
+	//TODO
 	public CharacterService takeDamages(int amount, int stun);
 	
 }
